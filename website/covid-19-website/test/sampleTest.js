a5223ca31c3359bc7fc7200404b89359e323b54a
Ahnaf Habib Khan <ahnafkhan70@gmail.com>
2022-02-03 11:26:56 -0500

    test name changed

--- website/covid-19-website/test/sampleTest.js
+++ website/covid-19-website/test/sampleTest.js
@@ -3,11 +3,15 @@
 import { assert } from 'chai';
 import { getUserByEmail, insertUser, deleteUserById } from"../src/backend/models/userModel.js";
 
-describe('Sample',function(){
+describe('GET USER BY EMAIL TEST',function(){
+    var server;
     it('sample test',function(){
         getUserByEmail("admin@gmail.com",(err, results) => {
             assert.equal(results["Email"], "admin@gmail.com");
         });
     });
+    // after(function(done){
+    //     process.exit(); // <-- THIS LOOKS REALLY BAD
+    //   });
 });
 // describe.close();
