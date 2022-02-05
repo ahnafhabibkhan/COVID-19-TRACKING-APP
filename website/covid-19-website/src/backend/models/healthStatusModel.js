e906e6adcfde930b077a8d6bbfc6c51c9a15955b
Tommy Soucy <soucy.tommy@gmail.com>
2022-02-04 21:57:48 -0500

    #15 Added health status API

---
+++ website/covid-19-website/src/backend/models/healthStatusModel.js
@@ -0,0 +1,74 @@
+// import connection
+import db from "../config/db.js";
+
+// Get Single health status by ID
+export const getLatestHealthStatusByID = (pid, result) => {
+    db.query("select * from healthstatus where PID = ? order by fillOutDate desc", [pid], (err, results) => {
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results[0]);
+        }
+    });
+}
+
+// Get health statuses by ID
+export const getHealthStatusesByID = (pid, result) => {
+    db.query("select * from healthstatus where PID = ?", [pid], (err, results) => {
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });
+}
+
+// Insert health status into Database
+export const insertHealthStatus = (data, result) => {
+    db.query("INSERT INTO healthstatus SET ?", [data], (err, results) => {
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });
+}
+
+// Delete healthstatus from Database
+export const deleteHealthStatusByPK = (args, result) => {
+    db.query("DELETE FROM healthstatus WHERE PID = ?, fillOutDate = ?", [args.pid, args.fillOutDate], (err, results) => {
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });
+}
+
+// Delete healthstatuses from Database
+export const deleteHealthStatusByID = (pid, result) => {
+    db.query("DELETE FROM healthstatus WHERE PID = ?", [pid], (err, results) => {
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });
+}
+
+// Update a health status
+export const updateHealthStatusByPK = (data, pid, fillOutDate, result) => {
+    db.query("UPDATE healthstatus SET ? WHERE PID = ?, fillOutDate = ?", [data, pid, fillOutDate], (err, results) => {
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });
+}
