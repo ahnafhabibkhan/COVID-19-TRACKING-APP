f793ee07a80d0e441959861207241acfe24344c4
MansiEdi2020 <mansiedi1980@gmail.com>
2022-02-10 01:12:35 -0500

    Update Profile page( add features and save/cancel btn)

--- website/covid-19-website/src/user-profile/profile-page.vue
+++ website/covid-19-website/src/user-profile/profile-page.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-image">
-      <v-btn small color="accent" elevation="0" @click="back"> back</v-btn>
-      <h1>Profile page</h1>
-  
+    <v-btn small color="accent" elevation="0" @click="back"> back</v-btn>
+    <h1>Profile page</h1>
+
     <v-container>
       <v-row>
         <v-col cols="12" md="6">
-          <v-text-field v-model="form.firstName" label="firstname" />
+          <v-text-field v-model="form.firstName" label="firstname" :autocomplete="false" />
         </v-col>
         <v-col cols="12" md="6">
           <v-text-field v-model="form.lastName" label="lastName" />
@@ -21,6 +21,14 @@
           <v-text-field v-model="form.bithDate" label="birthDate" type="date" />
         </v-col>
         <v-col cols="12" md="6">
+           <v-select
+           item-text="title"
+           item-value="value"
+          :items="roles"
+          label="role"
+        ></v-select>
+        </v-col>
+        <v-col cols="12" md="6">
           <v-text-field
             v-model="form.password"
             label="password"
@@ -28,11 +36,10 @@
           />
         </v-col>
 
-        <v-col cols="12" >
+        <v-col cols="12">
           <div class="d-flex justify-center">
-
-          <v-btn @click="save" width="150px" class="mx-2">save</v-btn>
-          <v-btn @click="back"  width="150px">cancel</v-btn>
+            <v-btn @click="save" width="150px" class="mx-2">save</v-btn>
+            <v-btn @click="back" width="150px">cancel</v-btn>
           </div>
         </v-col>
       </v-row>
@@ -45,6 +52,13 @@ export default {
   name: "Profile",
   data() {
     return {
+      roles:[
+              { title: "admin", value: 1 },
+        { title: "patient", value: 2 },
+        { title: "doctor", value: 3 },
+        { title: "health officer", value: 4 },
+        { title: "immigrant officer", value: 5 },
+      ],
       form: {
         firstName: null,
         lastName: null,
@@ -59,17 +73,34 @@ export default {
     back() {
       this.$router.go(-1);
     },
-    
-    async save(){
-      try{
-       const res= await this.$axios.put('http://localhost:5000/profile/${id}',this.form)
-       console.log('successsfull upadated', res);
 
-      }catch(err){
-        console.log('error ; save profile' , err);
+    async save() {
+      try {
+        const res = await this.$axios.put(
+          "http://localhost:5000/profile/${id}",
+          this.form
+        );
+        console.log("successsfull upadated", res);
+      } catch (err) {
+        console.log("error ; save profile", err);
       }
-    }
+    },
+    async get() {
+      const id = this.$route.params.id;
+      try {
+        const res = await this.$axios.get(
+          `http://localhost:5000/profile/${id}`
+        );
+        console.log("successsfull upadated", res);
+        this.form = res;
+      } catch (err) {
+        console.log("error ; save profile", err);
+      }
+    },
   },
+  mounted(){
+    this.get()
+  }
 };
 </script>
 <style>
