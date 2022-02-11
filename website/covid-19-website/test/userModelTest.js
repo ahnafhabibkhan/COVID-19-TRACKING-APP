ce811a7336ffb61bafce14fffcad2114597aaf56
HWJFish <wwc641810324@gmail.com>
2022-02-11 08:43:20 -0500

    partly fixed the test

--- website/covid-19-website/test/userModelTest.js
+++ website/covid-19-website/test/userModelTest.js
@@ -25,11 +25,33 @@ describe('insert and delete test',function(){
         getUserByEmail("testOnly@gmail.com",(err, results) => {
             assert.equal(results, undefined);
         });
-        insertUser("testOnly@gmail.com",(err, results) => {
-            assert.equal(results, undefined);
+        insertUser({
+            Email: "testOnly@gmail.com",
+            FirstName: "testFirst",
+            LastName: "testLast",
+            Telephone: "4504664545", 
+            Address: "123 Main", 
+            Role: "Patient", 
+            Password: "TestPass", 
+            City: "Montreal", 
+            Country: "Canada"},(err, results) => {
+
         });
-        getUserByEmail("notAvaliable.com",(err, results) => {
+        
+        getUserByEmail("testOnly@gmail.com",(err, results) => {
             assert.equal(results['Email'], "testOnly@gmail.com");
+            const id=results['UserID'];
+            console.log("checking id after adding: ",id);
+            
+            console.log("checking id out side: ",id);
+            
+            deleteUserById(id,(err, results) => {
+                console.log("checking delete by id: ",id);
+             });
+        });
+        
+        getUserByEmail("testOnly@gmail.com",(err, results) => {
+            assert.equal(results, undefined);
         });
     });
     
