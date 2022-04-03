880fc219d479c6f450da24d98f7fb9cce122aa08
Yousha <youshahyatoolla@gmail.com>
2022-04-03 15:43:32 -0400

    Now displaying correct number of users and improved UI

--- website/covid-19-website/src/admin/Dashboard.vue
+++ website/covid-19-website/src/admin/Dashboard.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="dashboard">
     <div class="different-users">
-      <div class="stats"><h3>Number of doctors: 3</h3></div>
-      <div class="stats"><h3>Number of patients: 12</h3></div>
-      <div class="stats"><h3>Number of health officials: 7</h3></div>
-      <div class="stats"><h3>Number of immigration officers: 1</h3></div>
+      <div class="stats">
+        <h3>Number of doctors: {{ numberOfDoctors }}</h3>
+      </div>
+      <div class="stats">
+        <h3>Number of patients: {{ numberOfPatients }}</h3>
+      </div>
+      <div class="stats">
+        <h3>Number of health officials: {{ numberOfHOs }}</h3>
+      </div>
+      <div class="stats">
+        <h3>Number of immigration officers: {{ numberOfIOs }}</h3>
+      </div>
     </div>
     <div class="bottom-part">
       <div class="pie-chart-stats">
@@ -47,10 +55,15 @@ export default {
           },
         ],
       },
+      numberOfDoctors: 0,
+      numberOfPatients: 0,
+      numberOfHOs: 0,
+      numberOfIOs: 0,
     };
   },
   created() {
     this.getChartData();
+    this.getNumberOfUsers();
   },
   methods: {
     onPatientsClick() {
@@ -95,6 +108,29 @@ export default {
         console.log(err);
       }
     },
+    //Get number of users
+    async getNumberOfUsers() {
+      //Get number of patients
+      const patients = await axios.post(`http://localhost:5000/users`, {
+        Role: "Patient",
+      });
+      this.numberOfPatients = patients.data.length;
+      //Get number of doctors
+      const doctors = await axios.post(`http://localhost:5000/users`, {
+        Role: "Doctor",
+      });
+      this.numberOfDoctors = doctors.data.length;
+      //Get number of immigration officers
+      const IOs = await axios.post(`http://localhost:5000/users`, {
+        Role: "ImmigrationOfficer",
+      });
+      this.numberOfIOs = IOs.data.length;
+      //Get number of health officials
+      const HOs = await axios.post(`http://localhost:5000/users`, {
+        Role: "HealthOfficial",
+      });
+      this.numberOfHOs = HOs.data.length;
+    },
   },
 };
 </script>
@@ -107,17 +143,27 @@ export default {
 .different-users {
   width: 100%;
   height: 50%;
+  text-align: center;
 }
 .stats {
   padding-top: 50px;
   margin-left: 63px;
+  margin-right: 63px;
   display: inline-table;
-  border: 4px solid rgb(56, 54, 54);
+  border: 4px solid rgb(59, 147, 248);
+  border-radius: 25px;
+  background: rgb(144, 196, 255);
   height: 210px;
   width: 210px;
 }
 .pie-chart-stats {
-  margin-top: 30px;
-  border: 4px solid rgb(56, 54, 54);
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 5%;
+  border: 4px solid rgb(59, 147, 248);
+  border-radius: 25px;
+  background: rgb(144, 196, 255);
+  width: 500px;
+  text-align: center;
 }
 </style>
