87f323ee011596ccb9bd76e22c116997e4c84d8a
Yousha <youshahyatoolla@gmail.com>
2022-04-06 18:26:19 -0400

    About and contact pages

--- website/covid-19-website/src/about/Index.vue
+++ website/covid-19-website/src/about/Index.vue
@@ -1,7 +1,12 @@
 <template>
   <v-row justify="center">
     <v-col cols="12">
-      <h1 class="white--text">about us</h1>
+      <h1 class="white--text" style="text-align: center; margin-top: 15%">
+        The right platform used to keep track of your Covid Health Status. The
+        right platform to seek the right help with fighting Covid-19 with the
+        help of doctors available to you. You will have health officials and
+        doctors to your aid and to your community's aid.
+      </h1>
     </v-col>
   </v-row>
 </template>
